92ba350924707086c94bb9742ddb185b8d548282
thukutaJones <bsds2922@my.mzuni.ac.mw>
2024-10-03 23:20:14 +0100

    added idsr site

--- index.js
+++ index.js
@@ -27,12 +27,37 @@ app.get("/", async (req, res) => {
 
   const appData = {
     appName: "IMIS",
+    title: "Mzuni IMIS",
+    downloadColor: "bg-primary",
+    icon: "https://storage.googleapis.com/mzunidates/imis/icon.png",
     appDescription:
       "The Mzuni IMIS Portal is a centralized platform designed to streamline academic processes for students at Mzuzu University. Through the portal, students can easily register for courses, access real-time grade updates, manage course enrollment, and view fee payment details. Mzuni IMIS offers a personalized dashboard, providing students with important announcements, academic calendar updates, and the tools needed to efficiently manage their academic journey, ensuring they stay informed and organized throughout their time at the university.",
     appScreenshots: appPics,
     downloadLink: "https://storage.googleapis.com/mzunidates/imis/MZUNI%20IMIS.apk",
   };
   res.render("home", appData);
+});
+
+app.get("/idsrtutor", async (req, res) => {
+  const appPics = [];
+
+  for (let i = 1; i <= 8; i++) {
+    appPics.push(
+      `https://storage.googleapis.com/mzunidates/idsr/page${i}.jpg`
+    );
+  }
+
+  const appData = {
+    appName: "IDSR Tutor",
+    title: "IDSR Tutor",
+    downloadColor: "items-center justify-center bg-blue-600 w-full p-2 mt-6 rounded-2xl",
+    icon: "https://storage.googleapis.com/mzunidates/idsr/icon.png",
+    appDescription:
+      "The IDSR Tutor is an innovative platform designed to empower health workers by leveraging artificial intelligence to access vital information efficiently. Through the app, users can quickly search for disease surveillance data, guidelines, and best practices tailored to their specific needs. IDSR Tutor provides personalized recommendations, real-time updates on health trends, and access to training resources, ensuring that health workers are well-equipped to respond to public health challenges. With its user-friendly interface and comprehensive database, IDSR Tutor streamlines the process of finding critical information, enabling health professionals to make informed decisions and enhance their impact on community health.",
+    appScreenshots: appPics,
+    downloadLink: "https://storage.googleapis.com/mzunidates/idsr/idsrtutor.apk",
+  };
+  res.render("idsr", appData);
 });
 
 app.listen(port, "0.0.0.0");
